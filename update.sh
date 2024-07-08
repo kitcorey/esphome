49fed5e917d6b52dce6f83882901110bc8c18323
Kit Corey <kitcorey@users.noreply.github.com>
2024-07-08 16:58:42 -0600

    pip-compile --upgrade, add platform esphom and OTA password

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ docker run --rm \
  -v "${PWD}/.cache":/cache \
  -v "${PWD}/.build":/build \
  -v "${HOME}/repos/airgradient_esphome":"${HOME}/repos/airgradient_esphome" \
+ --pull always \
  --workdir /config \
  -e PLATFORMIO_CORE_DIR=/cache/.plattformio \
  -e PLATFORMIO_GLOBALLIB_DIR=/cache/.plattformioLibs \
