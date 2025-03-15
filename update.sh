0939f1698b806c513a765206e0ff4e44a633357e
Kit Corey <kitcorey@users.noreply.github.com>
2025-03-15 09:02:08 -0600

    Address 2025.2 breaking changes
    https://github.com/esphome/esphome/pull/8118

--- update.sh
+++ update.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+# .cache is necessary for platformio to work properly with esp-idf
+# https://github.com/esphome/issues/issues/6520
 docker run --rm \
  -v "${PWD}":/config \
- -v "${PWD}/.cache":/cache \
- -v "${PWD}/.build":/build \
+ -v "${PWD}/.cache":/.cache \
+ -v "${PWD}/.build":/.build \
  -v "${HOME}/repos/airgradient_esphome":"${HOME}/repos/airgradient_esphome" \
  --pull always \
  --workdir /config \
- -e PLATFORMIO_CORE_DIR=/cache/.plattformio \
- -e PLATFORMIO_GLOBALLIB_DIR=/cache/.plattformioLibs \
+ -e PLATFORMIO_CORE_DIR=/.cache/.plattformio \
+ -e PLATFORMIO_GLOBALLIB_DIR=/.cache/.plattformioLibs \
  -u $(id -u ${USER}):$(id -g ${USER}) \
  -it ghcr.io/esphome/esphome:latest update-all .
 
